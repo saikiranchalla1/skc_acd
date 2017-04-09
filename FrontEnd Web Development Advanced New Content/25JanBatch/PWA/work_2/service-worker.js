953760dc5f2b9a5688e19dba22aba02dd7d25c1f
SaiKiran <saikiranchalla@gmail.com>
2017-04-09 09:28:02 -0600

    further code

---
+++ FrontEnd Web Development Advanced New Content/25JanBatch/PWA/work_2/service-worker.js
@@ -0,0 +1,56 @@
+var cacheName = 'weatherPWA-step-6-1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/images/clear.png',
+  '/images/cloudy-scattered-showers.png',
+  '/images/cloudy.png',
+  '/images/fog.png',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+  '/images/partly-cloudy.png',
+  '/images/rain.png',
+  '/images/scattered-showers.png',
+  '/images/sleet.png',
+  '/images/snow.png',
+  '/images/thunderstorm.png',
+  '/images/wind.png'
+];
+
+self.addEventListener('install', function(e){
+    console.log('[ServiceWorker Install');
+
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    )
+})
+
+self.addEventListener('activate', function(e){
+    console.log('[ServiceWorker] activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList){
+            return Promise.all(keyList.map(function(key){
+                if(key !== cacheName){
+                    console.log('[ServiceWorker] removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+    console.log('[ServiceWorker] Fetch', event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            return response || fetch(event.request);
+        })
+    )
+})
